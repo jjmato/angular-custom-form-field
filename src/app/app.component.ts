f1f65a1b1b7ffed91cba6ca9b023b3f255cc6a68
jjmato <jjmatotorres@gmail.com>
2019-08-30 12:26:26 +0200

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   external : string;
+
+  constructor(
+    private fb: FormBuilder
+  ){
+    this.fb.group({
+      'custom': [
+        'init_value',
+        [
+          Validators.minLength(2),
+          Validators.maxLength(5),
+        ]
+      ]
+    })
+  }
 }
